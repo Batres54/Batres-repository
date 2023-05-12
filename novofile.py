e126378873eca13cfe133588bb0f7081f48ad6de
wvillam <wvillamm@gmail.com>
2023-05-12 17:05:55 -0500

    Some changes

--- novofile.py
+++ novofile.py
@@ -4,4 +4,8 @@ print("Como fue prrooooooo")
 
 print('entonces si yo escribo aca tu lo puedes ver???')
 
+print('ovio')
+print('Se puede hasta volar')
+groserias.fly
+
 """ Interesante como en Python se hacen notas """
